469cfb6c2597a6e852946f1ee148a9faff54bd6d
Aditya Mayukh Som <adi.kg2@gmail.com>
2025-03-16 13:16:56 +0530

    separate files for pair and trio

--- Solution.java
+++ Solution.java
@@ -1,171 +1,121 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
-import java.util.PriorityQueue;
+import java.util.Map;
 
-class Solution {
-    static class Pair {
-        int node;
-        int dist;
+public record Pair<U, V>(U first, V second) {}
+public record Trio<U, V, W>(U first, V second, W third) {}
 
-        Pair(final int node, final int dist) {
-            this.node = node;
-            this.dist = dist;
+public class Solution {
+    public int maxScore(List<List<Integer>> grid) {
+        int n = grid.size();
+        int m = grid.get(0).size();
+
+        List<Pair> valuesWithRow = new ArrayList<>();
+
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < m; c++) {
+                valuesWithRow.add(new Pair(grid.get(r).get(c), r));
+            }
         }
 
-        @Override
-        public boolean equals(Object o) {
-            if (this == o)
-                return true;
-            if (!(o instanceof Pair))
-                return false;
-            Pair c = (Pair) o;
-            return this.node == c.node;
-        }
+        // sort by value in descending order, higher values will be in front
+        Collections.sort(valuesWithRow, (a, b) -> b.first - a.first);
 
-        @Override
-        public String toString() {
-            return "Pair [node=" + node + ", dist=" + dist + "]";
-        }
-    }
+        // HashMap to store the maximum sum for each mask
+        Map<Integer, Integer> dp = new HashMap<>(); // mask -> maximum_sum
+        dp.put(0, 0); // Base case: mask 0 means no rows selected, score is 0
 
-    static class FastReader implements AutoCloseable {
-        BufferedReader br;
-        StringTokenizer st;
+        // Iterate over each value-row pair
+        for (Pair pair : valuesWithRow) {
+            int val = pair.first;
+            int row = pair.second;
 
-        public FastReader() throws IOException {
-            File file = new File("input.txt");
-            FileInputStream fis = new FileInputStream(file);
-            InputStreamReader isr = new InputStreamReader(fis);
-            this.br = new BufferedReader(isr);
-        }
-
-        String next() {
-            while (this.st == null || !st.hasMoreElements()) {
-                try {
-                    this.st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
+            Map<Integer, Integer> newDp = new HashMap<>(dp);
+            for (Map.Entry<Integer, Integer> entry : dp.entrySet()) {
+                int mask = entry.getKey();
+                int currentSum = entry.getValue();
+                int rowMask = 1 << row;
+                if ((mask & rowMask) == 0) {
+                    int newMask = mask | rowMask;
+                    newDp.put(newMask, Math.max(newDp.getOrDefault(newMask, 0), currentSum + val));
                 }
             }
-            return st.nextToken();
+
+            dp = newDp;
         }
 
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            String str = "";
-            try {
-                if (st.hasMoreTokens()) {
-                    str = st.nextToken("\n");
-                } else {
-                    str = br.readLine();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
-
-        @Override
-        public void close() throws IOException {
-            this.br.close();
-        }
+        return Collections.max(dp.values());
     }
 
-    public static List<Integer> shortestPath(int n, int m, int edges[][]) {
-        int[] dist = new int[n];
-        int[] prev = new int[n];
+    public int solve(int col, List<List<Integer>> grid, int[] set) {
+        if (col == 0) {
+            int maxPossible = 0;
 
-        int src = 0;
-        int dest = n - 1;
-
-        // in arraylist every pair contains node and weight
-        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            adj.add(new ArrayList<Pair>());
-            dist[i] = Integer.MAX_VALUE;
-            prev[i] = -1;
-        }
-
-        for (int[] edge : edges) {
-            adj.get(edge[0] - 1).add(new Pair(edge[1] - 1, edge[2]));
-            adj.get(edge[1] - 1).add(new Pair(edge[0] - 1, edge[2]));
-        }
-
-        // in priority queue, every pair contains node and dist
-        PriorityQueue<Pair> pq = new PriorityQueue<Pair>((e1, e2) -> (e1.dist - e2.dist));
-
-        pq.offer(new Pair(src, 0));
-        dist[src] = 0;
-
-        Pair p, newP;
-        int node, nbr, weight;
-        while (!pq.isEmpty()) {
-            p = pq.poll();
-            node = p.node;
-            for (Pair np : adj.get(node)) {
-                // each dist in np is effectively the weight
-                nbr = np.node;
-                weight = np.dist;
-                if ((dist[nbr] - weight) > dist[node]) {
-                    newP = new Pair(nbr, dist[node] + weight);
-                    pq.remove(newP);
-                    pq.offer(newP);
-                    dist[nbr] = newP.dist;
-                    prev[nbr] = node;
+            for (int v : grid.get(0)) {
+                if (set[v] == 0) {
+                    maxPossible = Math.max(maxPossible, v);
                 }
             }
+
+            return maxPossible;
         }
 
-        List<Integer> ans = new LinkedList<>();
+        int ans, maxAns = 0;
 
-        if (prev[dest] == -1) {
-            ans.add(-1);
-            return ans;
+        for (int v : grid.get(col)) {
+            if (set[v] == 0) {
+                set[v] = 1;
+                ans = solve(col - 1, grid, set);
+                maxAns = Math.max(v + ans, maxAns);
+                set[v] = 0;
+            }
         }
 
-        int temp = dest;
-        while (temp != -1) {
-            ans.add(0, temp + 1);
-            temp = prev[temp];
-        }
+        // In case in one middle column, every value is taken
+        // we still want to consider going to the next column
+        // without taking any value at all.
+        ans = solve(col - 1, grid, set);
+        maxAns = Math.max(ans, maxAns);
 
-        return ans;
+        return maxAns;
     }
 
-    public static void main(String[] args) {
-        try (FastReader reader = new FastReader();) {
-            // Solution sol = new Solution();
-            int n = reader.nextInt();
-            int m = reader.nextInt();
-            int[][] edges = new int[m][3];
-            for (int i = 0; i < m; ++i) {
-                edges[i][0] = reader.nextInt();
-                edges[i][1] = reader.nextInt();
-                edges[i][2] = reader.nextInt();
+    public int maxScoreBacktrack(List<List<Integer>> grid) {
+        int n = grid.size();
+        int[] set = new int[101];
+        return solve(n - 1, grid, set);
+    }
+
+    public int numTeams(int[] rating) {
+        int res = 0;
+
+        for (int i = 0; i < rating.length; ++i) {
+            int leftSmaller = 0;
+            int leftBigger = 0;
+            int rightSmaller = 0;
+            int rightBigger = 0;
+
+            for (int j = i - 1; j >= 0; --j) {
+                if (rating[j] > rating[i]) {
+                    leftBigger++;
+                } else if (rating[j] < rating[i]) {
+                    leftSmaller++;
+                }
             }
-            System.out.println(shortestPath(n, m, edges));
-        } catch (IOException e) {
-            System.out.println("IO Exception occurred while reading input.");
-            e.printStackTrace();
+
+            for (int k = i + 1; k < rating.length; k++) {
+                if (rating[k] < rating[i]) {
+                    rightSmaller++;
+                } else if (rating[k] > rating[i]) {
+                    rightBigger++;
+                }
+            }
+
+            res += ((leftSmaller * rightBigger) + (leftBigger * rightSmaller));
         }
+
+        return res;
     }
 }
