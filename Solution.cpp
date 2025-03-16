8651500903913bff57059d4d78d1145389cb8fd1
Aditya Mayukh Som <adi.kg2@gmail.com>
2025-03-16 16:38:52 +0000

    added cpp template

---
+++ Solution.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename U, typename V>
+struct Pair
+{
+    U first;
+    V second;
+};
+
+template <typename U, typename V, typename W>
+struct Trio
+{
+    U first;
+    V second;
+    W third;
+};
+
+class Solution
+{
+
+};
+
+int main()
+{
+    
+}
